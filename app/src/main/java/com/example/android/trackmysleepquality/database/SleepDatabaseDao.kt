744ab930b52c8c110f74943f745fe9a800688ca5
Brais Cidras <braisomary@gmail.com>
2020-03-26 13:53:32 +0200

    Adding SleepDatabaseDao

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,4 +16,33 @@
 
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface SleepDatabaseDao {
+    @Insert
+    fun insert(night: SleepNight)
+
+    @Update
+    fun update(night: SleepNight)
+
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: Long): SleepNight?
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    // Since is the list of SleepNight is declared as LiveData,
+    // the list will be automatically updated after any change of data is made in the database
+    // there is no need for fetching the data again from the database
+    // since the observable is doing this job for us
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+}
