5ffcfae845608cf0c0c69918d6c4bdbdbc49d5a9
Brais Cidras <braisomary@gmail.com>
2020-03-27 11:07:00 +0200

    Adding Coroutines for Long-running operations

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -18,7 +18,12 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.formatNights
+import kotlinx.coroutines.*
 
 /**
  * ViewModel for SleepTrackerFragment.
@@ -26,5 +31,98 @@ import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 class SleepTrackerViewModel(
         val database: SleepDatabaseDao,
         application: Application) : AndroidViewModel(application) {
+
+    private var viewModelJob = Job()
+
+    // Defining scope determines which threat the co-routine will be run on and needs to know the job as well
+    // Our dispatcher is Dispatchers.Main, meaning co-routines launched in the UI scope will run on the main thread
+    private val uiScope = CoroutineScope(Dispatchers.Main +  viewModelJob)
+
+    // Current night
+    private var tonight = MutableLiveData<SleepNight?>()
+
+    // Returning LiveDate from Room, so any change made on the database will de displayed automatically, we don't need to do anything else
+    private val nights = database.getAllNights()
+
+    val nightsString = Transformations.map(nights) { nights ->
+        formatNights(nights, application.resources)
+    }
+
+    init {
+        initializeTonight()
+    }
+
+    private fun initializeTonight() {
+        // Launching a co-routine in uiScope without blocking the current thread
+        uiScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    // Returns null if there is no current started sleepNight
+    // Marked as suspend because we want to call it inside the co-routine and don't block
+    private suspend fun getTonightFromDatabase():  SleepNight? {
+        // We actually create another co-routine in the IO context
+        return withContext(Dispatchers.IO) {
+            var night = database.getTonight()
+
+            //  If the start and end times are the not the same, meaning, the night has already been completed
+            if (night?.endTimeMilli != night?.startTimeMilli) {
+                night = null
+            }
+            night
+        }
+    }
+
+    // Start Button click handler
+    fun onStartTracking() {
+        uiScope.launch {
+            val newNight = SleepNight()
+            insert(newNight)
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+
+    private suspend fun insert(night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.insert(night)
+        }
+    }
+
+    // Stop Button click handler
+    fun onStopTracking() {
+        uiScope.launch {
+            val oldNight = tonight.value ?: return@launch
+            oldNight.endTimeMilli = System.currentTimeMillis()
+            update(oldNight)
+        }
+    }
+
+    private suspend fun update(night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.update(night)
+        }
+    }
+
+    // Clear Button click handler
+    fun onClear() {
+        uiScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+
+    private suspend fun clear() {
+        withContext(Dispatchers.IO) {
+            database.clear()
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        // Cancelling all co-routines
+        viewModelJob.cancel()
+    }
 }
 
